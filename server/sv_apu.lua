2299402068d8b7f3c25b774002274ed6bd78c3d3
v4mpp <101105433+v4mmpp@users.noreply.github.com>
2022-03-13 16:38:52 +0400

    Update sv_apu.lua

--- server/sv_apu.lua
+++ server/sv_apu.lua
@@ -11,16 +11,16 @@ AddEventHandler('xed:buyitem', function(v, type)
 			
 	if (type) then
 		if (playerMoney.bank >= price) then
-			canBuy = true;
 			xPlayer.removeAccountMoney('bank', price)
+			canBuy = true;
 			TriggerClientEvent('esx:showAdvancedNotification', _source, 'Fleeca Bank', 'Virement', 'Paiement accepté d\'un montant de '..price..' ~g~$~s~ !', 'CHAR_BANK_FLEECA', 2)	
 		else
 			return TriggerClientEvent('esx:showAdvancedNotification', _source, 'Fleeca Bank', 'Virement', 'Paiement ~r~refusé~s~ !', 'CHAR_BANK_FLEECA', 2);
 		end
 	else
 		if (playerMoney.cash >= v.prix) then
-			canBuy = true;
 			xPlayer.removeMoney(v.prix)
+			canBuy = true;
 			TriggerClientEvent('esx:showAdvancedNotification', _source, 'Vendeur', 'Interraction', 'Et voilà, ~g~'..v.prix..'$ ~s~en plus dans ma caisse, merci de votre visite !', 'CHAR_CHEF', 9)
 		else
 			return TriggerClientEvent('esx:showAdvancedNotification', _source, 'Vendeur', 'Interraction', 'Vous n\'avez pas ~r~assez d\'argent pour effectuer cette achat~s~ !', 'CHAR_CHEF', 9);
